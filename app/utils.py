a88e651d0d78bbb1558a2443fdbd83539f603d6f
Mauvois <roger.mauvois@gmail.com>
2025-04-10 16:00:44 +0200

    test de liaison des branches

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,1 @@
 #test vide
-#test n° 2 para ver si ahora si
-#test n° 3 para ver si ahora si
-#test n° 4 ahora si normalemente
-#test n° 5 ahora si normalemente
-#test n° 6 ahora si normalemente
-#test n° 7 ahora si seguro
-#test n° 8 ahora si seguro neta
